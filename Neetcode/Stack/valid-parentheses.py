89c69d8f7d17274aa5ba35a97225784be9d098a5
Alex Felici <sandroms521@gmail.com>
2025-08-15 19:07:48 -0400

    Update valid-parentheses.py

--- Neetcode/Stack/valid-parentheses.py
+++ Neetcode/Stack/valid-parentheses.py
@@ -1,25 +1,23 @@
 
-
-def isValid(s: str) -> bool:
-  stack = []
-  closeToOpen = {
-    "}" : "{",
-    "]" : "[",
-    ")" : "("
-  }
-  for c in s:
-    if c in closeToOpen:
-      if stack and stack[-1] == closeToOpen[c]:
-        stack.pop()
+class Solution:
+  def isValid(self, s: str) -> bool:
+    stack = []
+    closeToOpen = {
+      "}" : "{",
+      "]" : "[",
+      ")" : "("
+    }
+    for c in s:
+      if c in closeToOpen:
+        if stack and stack[-1] == closeToOpen[c]:
+          stack.pop()
+        else:
+          return False
       else:
-        return False
-    else:
-      stack.append(c)
-  
-  return True
+        stack.append(c)
+    
+    return True
 
 
-
-
-print(isValid("{(}"))
+print(Solution.isValid(Solution, "[{}]"))
 
