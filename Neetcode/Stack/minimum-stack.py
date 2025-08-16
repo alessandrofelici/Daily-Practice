c8e632c1481ce9adb09f9e0a6a41555e73ef987c
Alex Felici <sandroms521@gmail.com>
2025-08-16 14:19:57 -0400

    Create minimum-stack.py

---
+++ Neetcode/Stack/minimum-stack.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+  def __init__(self):
+    self.stack = []
+    self.minStack = []
+
+  def push(self, val: int) -> None:
+    self.stack.append(val)
+    # Case 1: first value
+    if len(self.stack) is 1:
+      self.minStack.append(val)
+    # Case 2: existing values (compare)
+    else:
+      if val < self.minStack[-1]:
+        self.minStack.append(val)
+      else:
+        self.minStack.append(self.minStack[-1])
+
+  def pop(self) -> None:
+    self.stack.pop()
+    self.minStack.pop()
+
+  def top(self) -> int:
+    return self.stack[-1]
+
+  def getMin(self) -> int:
+    return self.minStack[-1]
