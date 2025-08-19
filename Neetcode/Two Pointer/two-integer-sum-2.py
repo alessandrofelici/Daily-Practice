739b0fbdc70d6f93c50b729eedfde95bbe485e98
Alex Felici <sandroms521@gmail.com>
2025-08-19 14:07:16 -0400

    Update two-integer-sum-2.py

--- Neetcode/Two Pointer/two-integer-sum-2.py
+++ Neetcode/Two Pointer/two-integer-sum-2.py
@@ -1,12 +1,18 @@
 class Solution:
     def twoSum(self, numbers: list[int], target: int) -> list[int]:
-        for i in range(len(numbers)):
-            for j in range(len(numbers)):
-                if i == j:
-                    continue
-                if numbers[i] + numbers[j] == target:
-                  return [i+1,j+1]
+        min, max = 0, len(numbers)-1
+
+        while min < max:
+            curSum = numbers[min] + numbers[max]
+            if curSum > target:
+                max -= 1
+            if curSum < target:
+                min += 1
+            if curSum == target:
+                return [min+1, max+1]
+            
+        return []
 
 test_case = Solution()
-ans = test_case.twoSum([1,1,2,4], 2)
+ans = test_case.twoSum([0,0,0,1,2,4,4], 3)
 print(ans)
