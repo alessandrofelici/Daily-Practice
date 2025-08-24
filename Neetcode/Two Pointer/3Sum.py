bb50e523a07d0a1ac66ca46e9d89bcdb0c3065e0
Alex Felici <sandroms521@gmail.com>
2025-08-24 12:54:49 -0400

    Update 3Sum.py

--- Neetcode/Two Pointer/3Sum.py
+++ Neetcode/Two Pointer/3Sum.py
@@ -5,18 +5,20 @@ class Solution:
 
         for i in range(len(nums) - 2):
             j, k = i + 1, len(nums) - 1
-            sum = nums[i] + nums[j] + nums[k]
-            while k != j:
-                if sum > 0 and k > -1:
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            while k > j:
+                sum = nums[i] + nums[j] + nums[k]
+                if sum > 0:
                     k -= 1
-                if sum < 0 and j < len(nums):
+                if sum < 0:
                     j += 1
                 if sum == 0:
                     triplets.append([nums[i], nums[j], nums[k]])
-                    break
-                sum = nums[i] + nums[j] + nums[k]
+                    j += 1
+                    while nums[j] == nums[j - 1] and j < k:
+                        l += 1
             
-        
         return triplets
 
 nums = [-1,0,1,2,-1,-4]
