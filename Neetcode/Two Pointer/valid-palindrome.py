730704db9a522a7531632efb614c4858c001264c
Alex Felici <sandroms521@gmail.com>
2025-09-01 11:25:39 -0400

    Update valid-palindrome.py

--- Neetcode/Two Pointer/valid-palindrome.py
+++ Neetcode/Two Pointer/valid-palindrome.py
@@ -1,8 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = list(filter(lambda x: x.isalnum(), s))
-        length = len(s)
-        for i in range(math.ceil(length/2)):
-              if s[i].lower() != s[length-i-1].lower():
-                  return False
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not s[l].isalnum():
+              l += 1
+            while r > l and not s[r].isalnum():
+              r -= 1
+            if s[l].lower() != s[r].lower():
+               return False
+            
+            l, r = l + 1, r - 1
+        
         return True
+    
+sol = Solution()
+print(sol.isPalindrome("Was it a car or a cat I saw?"))
