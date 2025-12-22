60300ae9d76da28335caf3da3e750d40141e0591
Alessandro Felici <sandroms521@gmail.com>
2025-12-22 18:56:33 -0500

    KthLargest

--- Neetcode/Heap-and-PQ/kthlargest.py
+++ Neetcode/Heap-and-PQ/kthlargest.py
@@ -1,7 +1,21 @@
+import heapq
+import math
+
 class KthLargest:
 
-    def __init__(self, k: int, nums: List[int]):
-        
+    def __init__(self, k: int, nums: list[int]):
+        self.nums = nums
+        self.k = k
+        heapq.heapify(self.nums)
 
     def add(self, val: int) -> int:
+        heapq.heappush(self.nums, val)
+        nums = self.nums
+        while len(nums) > self.k:
+            heapq.heappop(nums)
+        return nums[0]
+
         
+sol = KthLargest(3, [4, 5, 8, 2])
+print(sol.add(3))
+print(sol.add(5))
